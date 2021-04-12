52d73ff29ef95a0b6844153f9d84ae9e318ad8eb
Darren Hague <darren.hague@fortybeans.com>
2021-04-12 22:44:52 +0100

    Working Sufferfest pad
    Keys light up more when pressed
    Correct keyboard actions are typed

--- main.py
+++ main.py
@@ -32,16 +32,16 @@ lit = 0
 last_button_states = 0
 colour_index = 0
 RED = (0x80, 0x00, 0x00)
-RED0 = (0x10, 0x00, 0x00)
-RED1 = (0x20, 0x00, 0x00)
-RED2 = (0x30, 0x00, 0x00)
-RED3 = (0x40, 0x00, 0x00)
-RED4 = (0x50, 0x00, 0x00)
-RED5 = (0x60, 0x00, 0x00)
-RED6 = (0x70, 0x00, 0x00)
-RED7 = (0x80, 0x00, 0x00)
-RED8 = (0x90, 0x00, 0x00)
-RED9 = (0xA0, 0x00, 0x00)
+RED0 = (0x20, 0x00, 0x00)
+RED1 = (0x30, 0x00, 0x00)
+RED2 = (0x40, 0x00, 0x00)
+RED3 = (0x50, 0x00, 0x00)
+RED4 = (0x60, 0x00, 0x00)
+RED5 = (0x70, 0x00, 0x00)
+RED6 = (0x80, 0x00, 0x00)
+RED7 = (0x90, 0x00, 0x00)
+RED8 = (0xA0, 0x00, 0x00)
+RED9 = (0xB0, 0x00, 0x00)
 BLUE = (0x00, 0x00, 0x80)
 WHITE = (0x80, 0x80, 0x80)
 BLACK = (0x00, 0x00, 0x00)
@@ -51,35 +51,47 @@ MAGENTA = (0x80, 0x00, 0x80)
 R=0
 G=1
 B=2
-BUTTONS = [(RED0, "0"), (RED1, "1"), (RED2, "2"), (RED3, "3"),
-           (RED4, "4"), (RED5, "5"), (RED6, "6"), (RED7, "7"),
-           (RED8, "8"), (RED9, "9"), (WHITE, "UP"), (CYAN, "`"),
-           (MAGENTA, "m"), (BLACK, None), (WHITE, "DOWN"), (YELLOW, " "),
+BUTTONS = [(RED0, Keycode.KEYPAD_ZERO),
+           (RED1, Keycode.KEYPAD_ONE),
+           (RED2, Keycode.KEYPAD_TWO),
+           (RED3, Keycode.KEYPAD_THREE),
+
+           (RED4, Keycode.KEYPAD_FOUR),
+           (RED5, Keycode.KEYPAD_FIVE),
+           (RED6, Keycode.KEYPAD_SIX),
+           (RED7, Keycode.KEYPAD_SEVEN),
+
+           (RED8, Keycode.KEYPAD_EIGHT),
+           (RED9, Keycode.KEYPAD_NINE),
+           (WHITE, Keycode.UP_ARROW),
+           (CYAN, Keycode.GRAVE_ACCENT),
+
+           (MAGENTA, Keycode.M),
+           (BLACK, None),
+           (WHITE, Keycode.DOWN_ARROW),
+           (YELLOW, Keycode.SPACEBAR),
           ]
 COL=0
 KEY=1
 
-for i in range(0, num_pixels):
+def button_colour(i):
     r = BUTTONS[i][COL][R]
     g = BUTTONS[i][COL][G]
     b = BUTTONS[i][COL][B]
-    pixels[i] = (r, g, b)
+    return (r, g, b)
+
+def bright(rgb):
+    return ((int)(rgb[0]*1.5), (int)(rgb[1]*1.5), (int)(rgb[2]*1.5))
+
+def dim(rgb):
+    return ((int)(rgb[0]*0.5), (int)(rgb[1]*0.5), (int)(rgb[2]*0.5))
+
+for i in range(0, num_pixels):
+    pixels[i] = dim(button_colour(i))
 
 # Works up to here
 
 # TODO: Keyboard stuff
-
-def colourwheel(pos):
-    if pos < 0 or pos > 255:
-        return (0, 0, 0)
-    if pos < 85:
-        return (255 - pos * 3, pos * 3, 0)
-    if pos < 170:
-        pos -= 85
-        return (0, 255 - pos * 3, pos * 3)
-    pos -= 170
-    return (pos * 3, 0, 255 - pos * 3)
-
 
 def read_button_states(x, y):
     pressed = [0] * 16
@@ -98,137 +110,20 @@ def read_button_states(x, y):
 held = [0] * 16
 while True:
     pressed = read_button_states(0, 16)
+    button_pressed = False
+    for i in range(0, num_pixels):
+        if pressed[i]:
+            print("Button pressed", i)
+            button_pressed = True
+            pixels[i] = bright(button_colour(i))
 
-    if pressed[0]:
-        pixels[0] = colourwheel(0 * 16)  # Map pixel index to 0-255 range
+            if not held[i]:
+                kbd.send(BUTTONS[i][KEY])
+                held[i] = 1
 
-        if not held[0]:
-            layout.write("volu")
-            kbd.send(Keycode.ENTER)
-            # held[0] = 1
-
-    elif pressed[1]:
-        pixels[1] = colourwheel(1 * 16)  # Map pixel index to 0-255 range
-
-        if not held[1]:
-            layout.write("mpc next")
-            kbd.send(Keycode.ENTER)
-            held[1] = 1
-
-    elif pressed[2]:
-        pixels[2] = colourwheel(2 * 16)  # Map pixel index to 0-255 range
-
-        if not held[2]:
-            layout.write("rad4")
-            kbd.send(Keycode.ENTER)
-            held[2] = 1
-
-    elif pressed[3]:
-        pixels[3] = colourwheel(3 * 16)  # Map pixel index to 0-255 range
-
-        if not held[3]:
-            layout.write("mpc next")
-            kbd.send(Keycode.ENTER)
-            held[3] = 1
-
-    elif pressed[4]:
-        pixels[4] = colourwheel(4 * 16)  # Map pixel index to 0-255 range
-
-        if not held[4]:
-            layout.write("vold")
-            kbd.send(Keycode.ENTER)
-            # held[4] = 1
-
-    elif pressed[5]:
-        pixels[5] = colourwheel(5 * 16)  # Map pixel index to 0-255 range
-
-        if not held[5]:
-            layout.write("mpc prev")
-            kbd.send(Keycode.ENTER)
-            held[5] = 1
-
-    elif pressed[6]:
-        pixels[6] = colourwheel(6 * 16)  # Map pixel index to 0-255 range
-
-        if not held[6]:
-            layout.write("rad3")
-            kbd.send(Keycode.ENTER)
-            held[6] = 1
-
-    elif pressed[7]:
-        pixels[7] = colourwheel(7 * 16)  # Map pixel index to 0-255 range
-
-        if not held[7]:
-            layout.write("mpc prev")
-            kbd.send(Keycode.ENTER)
-            held[7] = 1
-
-    elif pressed[8]:
-        pixels[8] = colourwheel(8 * 16)  # Map pixel index to 0-255 range
-
-        if not held[8]:
-            layout.write("toggle_disp1")
-            kbd.send(Keycode.ENTER)
-            held[8] = 1
-
-    elif pressed[9]:
-        pixels[9] = colourwheel(9 * 16)  # Map pixel index to 0-255 range
-
-        if not held[9]:
-            layout.write("mpc stop")
-            kbd.send(Keycode.ENTER)
-            held[9] = 1
-
-    elif pressed[10]:
-        pixels[10] = colourwheel(10 * 16)  # Map pixel index to 0-255 range
-
-        if not held[10]:
-            layout.write("rad2")
-            kbd.send(Keycode.ENTER)
-            held[10] = 1
-
-    elif pressed[11]:
-        pixels[11] = colourwheel(11 * 16)  # Map pixel index to 0-255 range
-
-        if not held[11]:
-            layout.write("mpc stop")
-            kbd.send(Keycode.ENTER)
-            held[11] = 1
-
-    elif pressed[12]:
-        pixels[12] = colourwheel(12 * 16)  # Map pixel index to 0-255 range
-
-        if not held[12]:
-            layout.write("ssh pi\"192.168.9.97 picade_switch")
-            kbd.send(Keycode.ENTER)
-            held[12] = 1
-
-    elif pressed[13]:
-        pixels[13] = colourwheel(13 * 16)  # Map pixel index to 0-255 range
-
-        if not held[13]:
-            layout.write("mpc toggle")
-            kbd.send(Keycode.ENTER)
-            held[13] = 1
-
-    elif pressed[14]:
-        pixels[14] = colourwheel(14 * 16)  # Map pixel index to 0-255 range
-
-        if not held[14]:
-            layout.write("rad1")
-            kbd.send(Keycode.ENTER)
-            held[14] = 1
-
-    elif pressed[15]:
-        pixels[15] = colourwheel(15 * 16)  # Map pixel index to 0-255 range
-
-        if not held[15]:
-            layout.write("mpc toggle")
-            kbd.send(Keycode.ENTER)
-            held[15] = 1
-
-    else:  # Released state
+    if not button_pressed:
         for i in range(16):
-            pixels[i] = (0, 0, 0)  # Turn pixels off
+            pixels[i] = dim(button_colour(i))
             held[i] = 0  # Set held states to off
-        time.sleep(0.1)  # Debounce
+
+    time.sleep(0.1)  # Debounce
